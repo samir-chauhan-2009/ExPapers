03ce68c7f8d6ce9de5744920842e6ef2fe98f917
samir-chauhan-2009 <samir.chauhan.2009@gmail.com>
2017-05-30 01:19:20 +0530

    Working initial commit. Implemented basic spring security.

---
+++ ExPapers/WebContent/WEB-INF/admin.jsp
@@ -0,0 +1,28 @@
+<%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page session="true"%>
+<html>
+<body>
+	<h1>Title : ${title}</h1>
+	<h1>Message : ${message}</h1>
+
+	<c:url var="logoutUrl" value="/logout"/>
+	<form action="${logoutUrl}" method="post">
+    	<input type="submit" value="Logout"/>
+    	<input type="hidden" name="${_csrf.parameterName}" value="${_csrf.token}"/>
+	</form>
+
+	<script>
+		function formSubmit() {
+			document.getElementById("logoutForm").submit();
+		}
+	</script>
+
+	<c:if test="${pageContext.request.userPrincipal.name != null}">
+		<h2>
+			Welcome : ${pageContext.request.userPrincipal.name} | <a
+				href="javascript:formSubmit()"> Logout</a>
+		</h2>
+	</c:if>
+
+</body>
+</html>
---
+++ ExPapers/src/com/gontuseries/hellocontroller/HelloController.java
@@ -0,0 +1,67 @@
+package com.gontuseries.hellocontroller;
+
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@Controller
+public class HelloController{
+	
+	public HelloController() {
+		System.out.println("hello from controller");
+	}
+
+	@RequestMapping("/welcome")
+	protected ModelAndView helloWorld() throws Exception {
+		System.out.println("inside handle request internal method");
+		ModelAndView modelAndView = new ModelAndView("HelloPage");
+		modelAndView.addObject("welcomeMessage", "This is welcome message");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value = "/admin**", method = RequestMethod.GET)
+	public ModelAndView adminPage() {
+
+		ModelAndView model = new ModelAndView();
+		model.addObject("title", "Spring Security Hello World");
+		model.addObject("message", "This is protected page!");
+		model.setViewName("admin");
+
+		return model;
+
+	}
+	
+	//Spring Security see this :
+			@RequestMapping(value = "/login", method = RequestMethod.GET)
+			public ModelAndView login(
+				@RequestParam(value = "error", required = false) String error,
+				@RequestParam(value = "logout", required = false) String logout) {
+
+				ModelAndView model = new ModelAndView();
+				if (error != null) {
+					model.addObject("error", "Invalid username and password!");
+				}
+
+				if (logout != null) {
+					model.addObject("msg", "You've been logged out successfully.");
+				}
+				model.setViewName("login");
+
+				return model;
+
+			}
+			
+			/*@RequestMapping("/logout")
+			public String showLoggedout(){
+			    return "logout";
+			}*/
+	
+}
